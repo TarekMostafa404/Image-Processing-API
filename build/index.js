14008ecaa3448df616f40199ab47b256d0ef4423
Tarek Mostafa <80650764+TarekMostafa404@users.noreply.github.com>
2022-01-23 16:12:30 +0200

    ref

--- build/index.js
+++ build/index.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
-  return (mod && mod.__esModule) ? mod : { 'default': mod };
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, '__esModule', { value: true });
-const express_1 = __importDefault(require('express'));
-const images_1 = __importDefault(require('./routes/images'));
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const imageApi_1 = __importDefault(require("./routes/assets/imageApi"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.use('/', images_1.default);
+app.use('/', imageApi_1.default);
 app.listen(port, () => {
-  console.log(`server is running at http://localhost:${port}`);
+    console.log(`server is running at http://localhost:${port}`);
 });
 exports.default = app;
